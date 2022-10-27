361baf4dc1313b4d4bf35fb9c719429ee1eead1f
Nuraiym <nabdirasulkyzy@gmail.com>
2022-10-27 13:53:42 +0500

    square

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useRef } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const divRef = useRef();
+  const color = ["red", "green"];
+  useEffect(() => {
+    const interval = setInterval(() => {
+      divRef.current.style.background =
+        color[Math.round(Math.random() * color.length)];
+    }, 500);
+    return () => clearInterval(interval);
+  }, [color]);
+
+  return <div className="block" ref={divRef}></div>;
 }
 
 export default App;
